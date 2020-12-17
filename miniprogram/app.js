1daf40a711d0cfc4419773317d535e3f446fe4e5
闫紫微 <31801292@stu.zucc.edu.cn>
2020-12-17 09:45:52 +0800

    2020-12-16

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,15 +1,22 @@
 //app.js
 App({
+  globalData : {
+    userId:'',//用户id
+    userInfo:null,//用户信息
+    auth:{//授权信息
+      'scope.useInfo':false
+    },
+    logged:false//登录状态
+  },
   onLaunch: function () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        // env: 'test-wtt3j',
+        env: 'test-wtt3j',
         traceUser: true,
       })
     }
-
-    this.globalData = {}
+    
   }
 })
